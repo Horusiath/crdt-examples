3933a45e1bc7ceac58478695b37fa104d2b103f6
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2018-02-15 08:08:12 +0100

    initial commit: delta-based GCounter, PNCounter and GSet

---
+++ common.fsx
@@ -0,0 +1,57 @@
+/// The MIT License (MIT)
+/// Copyright (c) 2018 Bartosz Sypytkowski
+
+namespace Crdt 
+
+open System
+
+type ReplicaId = String
+type Ord = 
+    | Lt = -1  // lower
+    | Eq = 0   // equal
+    | Gt = 1   // greater
+    | Cc = 2   // concurrent
+
+[<RequireQualifiedAccess>]
+module Helpers =
+
+    /// Helper method for insert-or-update semantic for Map
+    let upsert k v fn map =
+        match Map.tryFind k map with
+        | None -> Map.add k v map
+        | Some v -> Map.add k (fn v) map
+
+    let tup2map (k, v) = Map.add k v Map.empty
+
+    /// General merge used on values wrapped with options
+    let mergeOption merge a b =
+        match a, b with
+        | Some x, Some y -> Some (merge x y)
+        | Some x, None   -> Some x
+        | None, Some y   -> Some y
+        | None, None     -> None
+   
+[<RequireQualifiedAccess>]     
+module Version =  
+    type VTime = Map<ReplicaId, int64>
+    let empty: VTime = Map.empty
+    let inc r (vv: VTime) = vv |> Helpers.upsert r 1L ((+)1L)
+    let merge vv1 vv2 =
+        vv2 |> Map.fold (fun acc k v2 -> Helpers.upsert k v2 (max v2) acc) vv1
+    let compare (a: VTime) (b: VTime): Ord = 
+        let valOrDefault k map =
+            match Map.tryFind k map with
+            | Some v -> v
+            | None   -> 0L
+        let akeys = a |> Map.toSeq |> Seq.map fst |> Set.ofSeq
+        let bkeys = b |> Map.toSeq |> Seq.map fst |> Set.ofSeq
+        (akeys + bkeys)
+        |> Seq.fold (fun prev k ->
+            let va = valOrDefault k a
+            let vb = valOrDefault k b
+            match prev with
+            | Ord.Eq when va > vb -> Ord.Gt
+            | Ord.Eq when va < vb -> Ord.Lt
+            | Ord.Lt when va > vb -> Ord.Cc
+            | Ord.Gt when va < vb -> Ord.Cc
+            | _ -> prev ) Ord.Eq
---
+++ convergent/delta/pncounter.fsx
@@ -0,0 +1,42 @@
+/// The MIT License (MIT)
+/// Copyright (c) 2018 Bartosz Sypytkowski
+
+namespace Crdt
+
+#load "gcounter.fsx"
+
+/// A delta-state based implementation of immutable increment/decrement counter.
+[<RequireQualifiedAccess>]
+module PNCounter =
+
+    type PNCounter = PNCounter of inc:GCounter.GCounter * dec:GCounter.GCounter
+    
+    /// Creates an empty PN-Counter.
+    let zero = PNCounter(GCounter.zero, GCounter.zero)
+    
+    /// Returns a value of provided PN-Counter.
+    let value (PNCounter(inc, dec)) = GCounter.value inc - GCounter.value dec
+    
+    /// Increments a PN-Counter using replica `r` tag.
+    let inc r (PNCounter(inc, dec)) = PNCounter(GCounter.inc r inc, dec)
+    
+    /// Decrements a PN-Counter using replica `r` tag.
+    let dec r (PNCounter(inc, dec)) = PNCounter(inc, GCounter.inc r dec)
+    
+    /// Merges two PN-Counter instances together, returning another PN-Counter as results of their convergence.
+    let rec merge (PNCounter(inc1, dec1)) (PNCounter(inc2, dec2)) =
+        PNCounter(GCounter.merge inc1 inc2, GCounter.merge dec1 dec2)
+            
+    /// Merges a PN-Counter delta with PN-Counter instance, returning new G-Counter instance.
+    let mergeDelta delta counter = merge counter delta
+        
+    /// Splits a given PN-Counter into PN-Counter with prunned delta and its delta, if provided.
+    let split (PNCounter(GCounter.GCounter(inc, a), GCounter.GCounter(dec, b))) = 
+        let delta =
+            match a, b with
+            | None, None -> None
+            | _, _       -> 
+                let inc = defaultArg a GCounter.zero
+                let dec = defaultArg b GCounter.zero
+                Some <| PNCounter(inc, dec)
+        PNCounter(GCounter.GCounter(inc, None), GCounter.GCounter(dec, None)), delta
