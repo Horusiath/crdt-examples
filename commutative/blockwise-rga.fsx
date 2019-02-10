ce5505871f842fd791a12abe3994ac4f85f37219
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2019-02-10 08:45:39 +0100

    first draft of blockwise RGA ops

--- commutative/blockwise-rga.fsx
+++ commutative/blockwise-rga.fsx
@@ -45,12 +45,13 @@ module RGArray =
       match pos with
       | None   -> acc
       | Some p -> 
-        let vertex = Map.find p blocks
-        match vertex.Body with
-        | Tombstone _ -> foldStep acc fn blocks vertex.Next
+        let block = Map.find p blocks
+        match block.Body with
+        | Tombstone _ -> foldStep acc fn blocks block.Next
         | Data data -> 
           let acc' = fn acc data
-          foldStep acc' fn blocks vertex.Next
+          foldStep acc' fn blocks block.Next
+    
 
     let head = (Map.find head blocks).Next
     (foldStep (ResizeArray()) (fun a d -> a.AddRange d; a) blocks head).ToArray()
@@ -73,52 +74,83 @@ module RGArray =
     Insert(block, blockId, pos)
   
   /// Creates an event that after `apply` will remove an element at given position from a RGArray.
-  let removeAt (pos: BlockId) (length: int) (RGA _) = Remove(pos, length)
+  let removeAt (pos: BlockId) (length: int) (RGA _) = 
+    Remove(pos, length) //TODO: if length is longer than block.Body length we need to split this removal into parts
 
   /// Applies operation (either created locally or from remote replica) to current RGArray.
   let apply op (RGA(seqNr, blocks)): RGArray<'a> =
     /// Check if the offset fits inside a given block, and split it in that case.
-    let rec maybeSplit blocks (at: int) blockId (block: Block<'a>) =
+    let rec split blocks (at: int) blockId (block: Block<'a>) =
       match block.Body with
       | Data d when at < d.Length -> 
         let (lvec, rvec) = Array.splitAt at d
-        let rid = (fst blockId, at)
+        let rid: BlockId = (fst blockId, at)
         let right = { Body = Data rvec; Next = block.Next; Link = block.Link }
         let left = { Body = Data lvec; Next = Some rid; Link = Some rid }
-        Some (left, right, rid)
+        (left, blockId, right, rid)
       | Data d ->
         let blockId' = block.Link.Value
         let block' = Map.find blockId' blocks
-        maybeSplit blocks (at - d.Length) blockId' block'
+        split blocks (at - d.Length) blockId' block'
       | Tombstone len when at < len -> 
         let rid = (fst blockId, at)
         let right = { Body = Tombstone (len - at); Next = block.Next; Link = block.Link }
         let left = { Body = Tombstone at; Next = Some rid; Link = Some rid }
-        Some (left, right, rid)
+        (left, blockId, right, rid)
       | Tombstone len -> 
         let blockId' = block.Link.Value
         let block' = Map.find blockId' blocks
-        maybeSplit blocks (at - len) blockId' block'
-    // let rec update value pos prev vertex blocks =
-    //   match vertex.Next with
-    //   | None ->
-    //     blocks
-    //     |> Map.add prev { vertex with Next = Some pos }
-    //     |> Map.add pos { Data = Some value; Next = None }
-    //   | Some n when n < pos ->
-    //     blocks
-    //     |> Map.add prev { vertex with Next = Some pos }
-    //     |> Map.add pos { Data = Some value; Next = Some n }
-    //   | Some n ->
-    //     let next = Map.find n blocks
-    //     update value pos n next blocks
+        split blocks (at - len) blockId' block'
+
+    let rec remove blocks at length =
+      match Map.tryFind at blocks with
+      | None -> 
+        let (position, offset) = at
+        let (left, leftId, right, rightId) = split blocks offset at (Map.find (position, 0) blocks)
+        let blocks' =
+          blocks
+          |> Map.add leftId left
+          |> Map.add rightId right
+        remove blocks' at length
+      | Some block ->
+        match block.Body with
+        | Data d when d.Length = length -> Map.add at { block with Body = Tombstone length} blocks
+        | Data d when d.Length > length ->
+          let (left, leftId, right, rightId) = split blocks (d.Length - length) at block
+          blocks
+          |> Map.add leftId { left with Body = Tombstone length }
+          |> Map.add rightId right
+        | Data d ->
+          let remaining = length - d.Length
+          let blocks' = Map.add at { block with Body = Tombstone d.Length } blocks
+          remove blocks' block.Link.Value remaining
+        | Tombstone len when len >= length -> blocks // idempotent do-nothing
+        | Tombstone len ->
+          remove blocks block.Link.Value (len - length)
 
     match op with
-    | Remove(at, length) ->
-      // let vertex = { Map.find at blocks with Data = None }
-      // RGA(seqNr, Map.add at vertex blocks)
     | Insert(value, at, after) ->
-      // let prev = Map.find after blocks
-      // let blocks' = update value at after prev blocks
-      // let seqNr' = max seqNr (fst at)
-      // RGA(seqNr', blocks')
+      match Map.tryFind after blocks with
+      | Some prev ->
+        let block = { Body = Data value; Next = prev.Next; Link = None }
+        let prev' = { prev with Next = Some at }
+        let blocks' =
+          blocks
+          |> Map.add after prev'
+          |> Map.add at block
+        let atSeqNr = fst (fst at)
+        RGA(max seqNr atSeqNr, blocks')
+      | None ->
+        let (position, offset) = after
+        let (left, leftId, right, rightId) = split blocks offset after (Map.find (position,0) blocks)
+        let block = { Body = Data value; Next = Some rightId; Link = None }
+        let blocks' =
+          blocks
+          |> Map.add leftId { left with Next = Some at }
+          |> Map.add at block
+          |> Map.add rightId right
+        let atSeqNr = fst (fst at)
+        RGA(max seqNr atSeqNr, blocks')
+    | Remove(at, length) ->
+      let blocks' = remove blocks at length
+      RGA(seqNr, blocks')
